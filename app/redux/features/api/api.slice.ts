2a59d10fde80e677edfa220a6c29d5cb5f5db77f
abdullahmia <abdullah149161@bsdi-bd.org>
2023-12-23 01:19:04 +0600

    fix: authentication with next-auth

--- app/redux/features/api/api.slice.ts
+++ app/redux/features/api/api.slice.ts
@@ -3,8 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/v1",
-    // baseUrl: "https://api-abdullahmia-dev.onrender.com/api/v1",
+    // baseUrl: "http://localhost:8000/api/v1",
+    baseUrl: "https://api-abdullahmia-dev.onrender.com/api/v1",
     prepareHeaders: async (headers, { getState }: any) => {
       const token = getState().auth.token;
       if (token) {
